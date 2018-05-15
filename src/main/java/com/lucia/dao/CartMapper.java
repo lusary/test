441e736ff9a7bc580fa54b6cd002df74dc711e60
lucia <2022466130@qq.com>
2018-05-15 15:41:37 +0800

    project init commit

---
+++ src/main/java/com/lucia/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.lucia.dao;
+
+import com.lucia.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
